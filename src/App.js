4a08bf626aae8bf9d71e472d0019dd7dca810749
liaoyu <liaoyu@toguw.com>
2019-08-19 14:46:23 +0800

    save

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  componentDidMount(){
+
+  }  
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-1 col-xs-offset-11">
+            <h1>Hello !!</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
