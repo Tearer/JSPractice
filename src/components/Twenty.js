dd3f1672bad396c610d2c7a0579cf2f3adfb7d24
liaoyu <liaoyu@toguw.com>
2019-09-20 18:04:14 +0800

    save

--- src/components/Twenty.js
+++ src/components/Twenty.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import * as utils from '../../utils';
+import { declareFunction } from '@babel/types';
 
 export default class Twenty extends Component{
 
@@ -368,6 +369,160 @@ export default class Twenty extends Component{
         // utils.EventUtil.addHandler(applicationCache, "updateready", function(){
         //     applicationCache.swapCache();
         // });
+
+        // utils.CookieUtil.set("name", "Nicholas", "/books/projs/", "http://localhost:3000", new Date("January 1, 2020"));
+        // utils.CookieUtil.set("book", "Professional Javastript");
+        // utils.CookieUtil.unset("name");
+        // utils.CookieUtil.unset("book"); 
+        // console.log(utils.CookieUtil.get("name"));
+        // console.log(utils.CookieUtil.get("book"));
+
+        // sessionStorage.book = "Professional Javascript";
+        // utils.EventUtil.addHandler(document, "storage", function(event){
+        //     console.log("Storage changed for " + event);
+        // })
+        // sessionStorage.setItem("name", "Nicholas");
+        // sessionStorage.setItem("name", "Bob");
+        // sessionStorage.removeItem("name");
+
+        var indexedDB = window.indexedDB || window.msIndexDB || window.mozIndexDB || window.webkiIndexdDB;
+        var request, database;
+        request = indexedDB.open("admin", "1.0");
+        request.onerror = function(event){
+            console.log("Something bad happened while trying to open: " + event.target.errorCode);
+        }
+        request.onsuccess = function(event){
+            database = event.target.result;
+            // database = request.result;
+
+            //已废弃
+            /*if (database.version !== "1.0"){
+                request = database.setVersion("1.0");
+                request.onerror = function(event){
+                    console.log("error to set version: " + event.target.errorCode);
+                };
+                request.onsuccess = function(event){
+                    console.log("Database initialization complete. Database name: " + database.name + ", Version: " + database.version);
+                };
+            } else {
+                console.log("Database already initialized. Database name: " + database.name + ", Version: " + database.version);
+            }*/
+
+            // var transaction = db.transaction("users");
+            // var IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction; 
+            // var transaction = database.transaction("users", IDBTransaction.READ_WRITE); //已废弃
+
+            // var request = database.transaction("users").objectStore("users").get("007");
+            // request.onerror = function(event){
+            //     console.log("Did not get the object!");
+            // };
+            // request.onsuccess = function(event){
+            //     var result = event.target.result;
+            //     console.log(result.firstName);    //"James"
+            // };
+
+            // var store = database.transaction("users",'readwrite').objectStore("users"),
+            //     request = store.openCursor();
+            // store.add(user2);
+
+            // var IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange;
+            // var onlyRange = IDBKeyRange.only("007"); 
+            // var lowerRange = IDBKeyRange.lowerBound("007");
+            // var lowerRange = IDBKeyRange.lowerBound("007", true);
+            // var upperRange = IDBKeyRange.upperRange("ace");
+            // var upperRange = IDBKeyRange.upperRange("ace", true);
+            // var boundRange = IDBKeyRange.bound("007", "ace");
+            // var boundRange = IDBKeyRange.bound("007", "ace", true);
+            // var boundRange = IDBKeyRange.bound("007", "ace", true, true);
+            // var boundRange = IDBKeyRange.bound("007", "ace", false, true);
+            // var store = database.transaction("users").objectStore("users"),
+            //     range = IDBKeyRange.bound("007", "ace"),
+            //     request = store.openCursor(range, "nextunique"); //"next", "nextunique", "prev", and "prevunique". The default is "next".
+            
+            // request.onsuccess = function(event){
+            //     var cursor = event.target.result;
+            //     if (cursor){
+            //         console.log("Key: " + cursor.key + ", Value: " + JSON.stringify(cursor.value));
+            //         cursor.continue(); //移动到下一项
+            //     } else {
+            //         console.log("Done!");
+            //     }
+            // };
+            // request.onsuccess = function(event){
+            //     var cursor = event.target.result,
+            //         value,
+            //         updateRequest;
+            //     if (cursor){
+            //         if (cursor.key == "007"){
+            //             value = cursor.value;
+            //             value.password = "mageic!";
+
+            //             updateRequest = cursor.update(value);
+            //             updateRequest.onsuccess = function(){
+            //             };
+            //             updateRequest.onerror = function(){
+            //             };
+            //         }
+            //     }
+            // };
+            // request.onsuccess = function(event){
+            //     var cursor = event.target.result,
+            //         value,
+            //         deleteRequest;
+            //     if (cursor){
+            //         if (cursor.key == "007"){
+            //             deleteRequest = cursor.delete();
+            //             deleteRequest.onsuccess = function(){
+            //             };
+            //             deleteRequest.onerror = function(){
+            //             };
+            //         }
+            //     }
+            // }
+
+            // var store = database.transaction("users").objectStore("users"),
+                // index = store.createIndex("username", "username", { unique: false });
+                // index = store.index("username"),
+                // request = index.openCursor();
+                // request = index.openKeyCursor();
+                // request = index.get("007");
+                // request = index.getKey("007");
+            
+            // var store = database.transaction("users").objectStore("users"),
+            //     indexNames = store.indexNames,
+            //     index,
+            //     i = 0,
+            //     len = indexNames.length;
+            // while(i < len){
+            //     index = store.index(indexNames[i++]);
+            //     console.log(index.name, index.keyPath, index.unique);
+            // }
+
+            // store.deleteIndex("username");
+
+
+        }
+        request.onupgradeneeded = function (event){
+            database = event.target.result;
+            var store = database.createObjectStore("users", {keyPath: "username"});
+            store.add(user);
+        }
+
+        var user = {
+            username: "007",
+            firstName: "James",
+            lastName: "Bond",
+            password: "foo"
+        }
+        var user2 = {
+            username: "008",
+            firstName: "PDD",
+            lastName: "Bond",
+            password: "foo"
+        }
+
+
+
 
 
 
