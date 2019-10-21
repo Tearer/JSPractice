c48fc8683e5ad373998fd0420d43599320dd804a
liaoyu <liaoyu@toguw.com>
2019-10-21 18:50:46 +0800

    save

--- src/components/GoodParts.js
+++ src/components/GoodParts.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React, {Component} from 'react';
 // import * as utils from '../../utils';
 
@@ -45,11 +46,67 @@ export default class GoodParts extends Component{
         var fibonacci = memoizer([0, 1], function (recur, n) {
             return recur (n - 1) + recur (n - 2);
         });
-        console.log(fibonacci(10));
+        // console.log(fibonacci(10));
         var factorial = memoizer([1, 1], function (recur, n) {
             return n * recur (n - 1);
         });
-        console.log(factorial(3));
+        // console.log(factorial(3));
+
+        if (typeof Object.beget !== 'function') {
+            Object.create = function (o) {
+                var F = function () {};
+                F.prototype = o;
+                return new F();
+            };
+        }
+
+        var add = function (a, b) {
+            return a + b;
+        };
+
+        var myObject = {
+            value: 0,
+            increment: function (inc) {
+                this.value += typeof inc === 'number' ? inc : 1;
+            }
+        };
+        myObject.increment();
+        // console.log(myObject.value);
+        myObject.increment(2);
+        // console.log(myObject.value);
+
+        var sum = add(3, 4);
+        // console.log(sum);
+
+        myObject.double = function () {
+            var that = this;
+            // console.log(this);
+            var helper = function () {
+                // console.log(this);
+                that.value = add(that.value, that.value);
+            };
+            helper();
+        };
+        myObject.double();
+        // console.log(myObject.value);
+
+        var Quo = function (string) {
+            this.status = string;
+        };
+        Quo.prototype.get_status = function () {
+            return this.status;
+        };
+        var myQuo = new Quo("confused");
+        // console.log(myQuo.get_status());
+
+        var array = [3, 4];
+        var sum = add.apply(null, array);
+        var statusObject = {
+            status: 'A-OK'
+        };
+        var status = Quo.prototype.get_status.apply(statusObject);
+
+
 
 
 
